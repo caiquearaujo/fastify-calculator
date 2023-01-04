15a8f947d7169dd5b0587df18c4ba568892546cc
Caique Araujo <caique@studiopiggly.com.br>
2023-01-04 11:28:50 -0300

    :sparkles: feat(calculator): Base de uma calculadora

---
+++ src/calcs/basecalculator.ts
@@ -0,0 +1,27 @@
+import { IPrivateCalculator, IPublicCalculator, TFnOperation } from '../types';
+
+export default class BaseCalculator<T = number>
+	implements IPrivateCalculator<T>
+{
+	private operations: Record<string, TFnOperation<T>> = {};
+
+	register(name: string, fn: TFnOperation<T>) {
+		this.operations[name] = fn;
+	}
+
+	perform(name: string, ...args: T[]): T {
+		if (name in this.operations) {
+			return this.operations[name](args);
+		}
+
+		throw new Error(`Operation "${name}" is not registered`);
+	}
+
+	available(): string[] {
+		return Object.keys(this.operations);
+	}
+
+	static create(): IPublicCalculator<number> {
+		throw new Error('You have to implement this method!');
+	}
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,12 @@
 export type TReducer<T = number> = (prev: T, curr: T) => T;
 export type TFnReduce<T = number> = (fn: TReducer<T>, args: T[]) => T;
 export type TFnOperation<T = number> = (args: T[]) => T;
+
+export interface IPublicCalculator<T = number> {
+	perform(name: string, ...args: T[]): T;
+	available(): string[];
+}
+
+export interface IPrivateCalculator<T = number> extends IPublicCalculator<T> {
+	register(name: string, fn: TFnOperation<T>): void;
+}
