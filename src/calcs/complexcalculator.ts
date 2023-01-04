ee8bca853a2663a918ab9d068c803c698364c3fe
Caique Araujo <caique@studiopiggly.com.br>
2023-01-04 11:29:45 -0300

    :sparkles: feat(calculator): Implementação de uma calculadora complexa

---
+++ src/calcs/complexcalculator.ts
@@ -0,0 +1,16 @@
+import { square } from '../operations';
+import { IPrivateCalculator, IPublicCalculator } from '../types';
+import BaseCalculator from './basecalculator';
+
+export default class ComplexCalculator
+	extends BaseCalculator<number>
+	implements IPrivateCalculator<number>
+{
+	static create(): IPublicCalculator<number> {
+		const obj = new ComplexCalculator();
+
+		obj.register('square', square);
+
+		return obj;
+	}
+}
