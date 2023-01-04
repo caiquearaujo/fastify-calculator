8ef2ed75e792a844265deecd9f50fddf52f48c19
Caique Araujo <caique@studiopiggly.com.br>
2023-01-04 11:30:31 -0300

    :sparkles: feat(calculator): Implementação da calculadora global

---
+++ src/calculator.ts
@@ -0,0 +1,22 @@
+import ComplexCalculator from './calcs/complexcalculator';
+import SimpleCalculator from './calcs/simplecalculator';
+import { IPublicCalculator } from './types';
+
+export default class Calculator {
+	private simpleCalc: IPublicCalculator<number>;
+
+	private complexCalc: IPublicCalculator<number>;
+
+	constructor() {
+		this.simpleCalc = SimpleCalculator.create();
+		this.complexCalc = ComplexCalculator.create();
+	}
+
+	simple() {
+		return this.simpleCalc;
+	}
+
+	complex() {
+		return this.complexCalc;
+	}
+}
